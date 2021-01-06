9c4bc69b7a7f2af7e93e0a45e5be5ae36092b5ab
yadoshlivyy <yadoshlivyy@gmail.com>
2021-01-06 06:47:13 +0000

    configured debug configuration

--- Part2/Game.cpp
+++ Part2/Game.cpp
@@ -1,17 +1,18 @@
 #include "Game.hpp"
 
-
 static const char *colors[7] = {BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN};
 /*--------------------------------------------------------------------------------
 								
 --------------------------------------------------------------------------------*/
-void Game::run() {
+void Game::run()
+{
 
 	_init_game(); // Starts the threads and all other variables you need
 	print_board("Initial Board");
-	for (uint i = 0; i < m_gen_num; ++i) {
+	for (uint i = 0; i < m_gen_num; ++i)
+	{
 		auto gen_start = std::chrono::system_clock::now();
-		_step(i); // Iterates a single generation 
+		_step(i); // Iterates a single generation
 		auto gen_end = std::chrono::system_clock::now();
 		m_gen_hist.push_back((double)std::chrono::duration_cast<std::chrono::microseconds>(gen_end - gen_start).count());
 		print_board(nullptr);
@@ -19,54 +20,62 @@ void Game::run() {
 	print_board("Final Board");
 	_destroy_game();
 }
-
-void Game::_init_game() {
+Game::Game(game_params) { ; }
+const vector<double> Game::gen_hist() const { ; }
+const vector<double> Game::tile_hist() const { ; }
+uint Game::thread_num() const { ; }
+Game::~Game() { ; }
+void Game::_init_game()
+{
 
 	// Create game fields - Consider using utils:read_file, utils::split
 	// Create & Start threads
 	// Testing of your implementation will presume all threads are started here
 }
 
-void Game::_step(uint curr_gen) {
+void Game::_step(uint curr_gen)
+{
 	// Push jobs to queue
-	// Wait for the workers to finish calculating 
-	// Swap pointers between current and next field 
-	// NOTE: Threads must not be started here - doing so will lead to a heavy penalty in your grade 
+	// Wait for the workers to finish calculating
+	// Swap pointers between current and next field
+	// NOTE: Threads must not be started here - doing so will lead to a heavy penalty in your grade
 }
 
-void Game::_destroy_game(){
-	// Destroys board and frees all threads and resources 
-	// Not implemented in the Game's destructor for testing purposes. 
+void Game::_destroy_game()
+{
+	// Destroys board and frees all threads and resources
+	// Not implemented in the Game's destructor for testing purposes.
 	// All threads must be joined here
-	for (uint i = 0; i < m_thread_num; ++i) {
-        m_threadpool[i]->join();
-    }
+	for (uint i = 0; i < m_thread_num; ++i)
+	{
+		m_threadpool[i]->join();
+	}
 }
 
 /*--------------------------------------------------------------------------------
 								
 --------------------------------------------------------------------------------*/
-inline void Game::print_board(const char* header) {
+inline void Game::print_board(const char *header)
+{
 
-	if(print_on){ 
+	if (print_on)
+	{
 
-		// Clear the screen, to create a running animation 
-		if(interactive_on)
+		// Clear the screen, to create a running animation
+		if (interactive_on)
 			system("clear");
 
 		// Print small header if needed
 		if (header != nullptr)
 			cout << "<------------" << header << "------------>" << endl;
-		
-		// TODO: Print the board 
 
-		// Display for GEN_SLEEP_USEC micro-seconds on screen 
-		if(interactive_on)
+		// TODO: Print the board
+
+		// Display for GEN_SLEEP_USEC micro-seconds on screen
+		if (interactive_on)
 			usleep(GEN_SLEEP_USEC);
 	}
-
 }
-
 
 /* Function sketch to use for printing the board. You will need to decide its placement and how exactly 
 	to bring in the field's parameters. 
@@ -83,7 +92,4 @@ inline void Game::print_board(const char* header) {
 			cout << u8"║" << endl;
 		}
 		cout << u8"╚" << string(u8"═") * field_width << u8"╝" << endl;
-*/ 
-
-
-
+*/
