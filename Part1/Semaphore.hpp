8f25fdd6111c8aed41cbc94e1d14fda486f64750
yadoshlivyy <yadoshlivyy@gmail.com>
2021-01-09 21:48:12 +0000

    new changes in semaphore

--- Part1/Semaphore.hpp
+++ Part1/Semaphore.hpp
@@ -8,7 +8,7 @@ class Semaphore
 public:
 	Semaphore();			 // Constructs a new semaphore with a counter of 0
 	Semaphore(unsigned val); // Constructs a new semaphore with a counter of val
-	~Semaphore();
+
 	void up();	 // Mark: 1 Thread has left the critical section
 	void down(); // Block untill counter >0, and mark - One thread has entered the critical section.
 
