23f754bc6b827c6736d9b8dcac4f1e6dbfe34bf3
yadoshlivyy <yadoshlivyy@gmail.com>
2021-01-09 23:40:24 +0000

    thread_api implemented

--- Part2/Thread.hpp
+++ Part2/Thread.hpp
@@ -5,21 +5,25 @@
 class Thread
 {
 public:
-	Thread(uint thread_id) 
+	Thread(uint thread_id) : m_thread_id(thread_id)
 	{
-		// Only places thread_id 
-	} 
-	virtual ~Thread() {} // Does nothing 
+		// Only places thread_id
+	}
+	virtual ~Thread() {} // Does nothing
 
 	/** Returns true if the thread was successfully started, false if there was an error starting the thread */
-	// Creates the internal thread via pthread_create 
+	// Creates the internal thread via pthread_create
 	bool start()
 	{
+		int ret_val =
+			!pthread_create(&m_thread, NULL, entry_func, (void *)this);
+		return ret_val;
 	}
 
 	/** Will not return until the internal thread has exited. */
 	void join()
 	{
+		pthread_join(m_thread, NULL);
 	}
 
 	/** Returns the thread_id **/
@@ -27,13 +31,18 @@ public:
 	{
 		return m_thread_id;
 	}
+
 protected:
-	// Implement this method in your subclass with the code you want your thread to run. 
+	// Implement this method in your subclass with the code you want your thread to run.
 	virtual void thread_workload() = 0;
 	uint m_thread_id; // A number from 0 -> Number of threads initialized, providing a simple numbering for you to use
 
 private:
-	static void * entry_func(void * thread) { ((Thread *)thread)->thread_workload(); return NULL; }
+	static void *entry_func(void *thread)
+	{
+		((Thread *)thread)->thread_workload();
+		return NULL;
+	}
 	pthread_t m_thread;
 };
 
